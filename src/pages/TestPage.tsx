d1ed6484746ea9ea2db45fcfb9f3ae595a324443
Sean Z <pathtolifegithub@gmail.com>
2021-08-12 06:45:57 +1200

    Test Page encryption decryption

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,15 +1,25 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { Button, Typography } from '@material-ui/core'
-import { toast } from 'react-toastify'
+import { Button, makeStyles, Typography } from '@material-ui/core'
 import Web3 from 'web3'
-import { Route, Switch, useHistory } from 'react-router-dom'
 import PageContainer from '../components/layout/PageContainer'
-import ContactAddPage from './Contacts/ContactAddPage'
+import { useWeb3Loader } from '../web3/injector-connector'
+import LoadingPanel from '../components/LoadingPanel'
+import { PrimaryButton, SecondaryButton } from '../components/Buttons'
+import { StyledTextField } from '../components/TextInput'
+import { useMetaMaskEncryption } from '../web3/encryptionDecryption'
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    marginTop: theme.spacing(4),
+  },
+}))
 
 const TestPage: React.FC = () => {
-  const history = useHistory()
-  const { error, account, library } = useWeb3React<Web3>()
+  const classes = useStyles()
+  const { account, library } = useWeb3React<Web3>()
+
+  const [text, setText] = useState('')
 
   const accountAddress = useMemo(() => {
     if (!account) return 'Metamask connection required'
@@ -17,32 +27,55 @@ const TestPage: React.FC = () => {
     return account
   }, [account])
 
-  useEffect(() => {
-    if (!error) return
-    toast.error(error.message)
-  }, [error])
+  const { isLoading } = useWeb3Loader(true)
+  const {
+    encrypt,
+    decrypt,
+    encryptionPublicKey,
+    requestMetaMaskPublicKey,
+  } = useMetaMaskEncryption()
 
   const handleSend = () => {
-    if (!account) return
-    library?.givenProvider
-      .request({
-        method: 'eth_getEncryptionPublicKey',
-        params: [account],
-      })
-      .then((res: string) => {
-        console.log(res)
-      })
+    if (!library) return
+  }
+
+  const handleEncrypt = () => {
+    if (!encryptionPublicKey) return
+    setText(encrypt(text))
+  }
+
+  const handleDecrypt = () => {
+    decrypt(text).then((result: string) => setText(result))
   }
 
   return (
     <PageContainer>
-      <Typography variant={'h2'}>AppPage</Typography>
-      <Typography variant={'body1'}>{accountAddress}</Typography>
-      <Button onClick={handleSend}>Send</Button>
-      <Switch>
-        <Route exact path={'/'} />
-        <Route path={'/add'} component={ContactAddPage} />
-      </Switch>
+      <Typography variant={'h2'}>Test Page</Typography>
+      <LoadingPanel isLoading={isLoading}>
+        <Typography variant={'caption'}>{accountAddress}</Typography>
+        <Button onClick={handleSend}>Send</Button>
+        <StyledTextField
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          multiline
+          minRows={3}
+        />
+        <PrimaryButton onClick={handleEncrypt} disabled={!encryptionPublicKey}>
+          Encrypt
+        </PrimaryButton>
+        <SecondaryButton onClick={handleDecrypt}>Decrypt</SecondaryButton>
+        <SecondaryButton onClick={requestMetaMaskPublicKey}>
+          Request Key
+        </SecondaryButton>
+        <Button
+          className={classes.button}
+          color={'primary'}
+          fullWidth
+          variant={'contained'}
+        >
+          Normal Button
+        </Button>
+      </LoadingPanel>
     </PageContainer>
   )
 }
